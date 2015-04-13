398f1e75677606d03aaa156deb5cbe57639b7a7d
kevinissun <kevinissun@163.com>
2015-04-13 14:14:40 +0800

    后端代码

--- admin/application/controllers/homepage.php
+++ admin/application/controllers/homepage.php
@@ -14,26 +14,75 @@ class Homepage extends CI_Controller
 	public function index()
 	{
 		$this->load->helper('url');
-		$this->load->view('homepage');
+
+		$this->load->view('homepage', $this->getAllData());
 	}
 
 	public function editIntroduce()
 	{
-		if (!session_id()) session_start();
-
 		$submitInfo = array($this->input->post('introduce1'), 
-			           		$this->input->post('introduce1'),
-			           		$this->input->post('introduce1'),
-			           		$this->input->post('introduce1'),
-			           		$this->input->post('introduce1'));
+			           		$this->input->post('introduce2'),
+			           		$this->input->post('introduce3'),
+			           		$this->input->post('introduce4'),
+			           		$this->input->post('introduce5'));
 
 	    $this->load->model('homeModel');
 
-	    var_dump($submitInfo);
-	    
 	    for ($i=0; $i < 5; $i++) { 
-	    	
+	    	if ($submitInfo[$i]) {
+	    		$this->homeModel->editIntroduce($submitInfo, $i);
+	    	}
 	    }
+
+	    $this->load->view('homepage', $this->getAllData());
+	}
+
+	public function editProject()
+	{
+		$submitInfo = array($this->input->post('project1'), 
+			           		$this->input->post('project2'),
+			           		$this->input->post('project3'),
+			           		$this->input->post('project4'),
+			           		$this->input->post('project5'));
+
+	    $this->load->model('homeModel');
+
+	    for ($i=0; $i < 5; $i++) { 
+	    	if ($submitInfo[$i]) {
+	    		$this->homeModel->editProject($submitInfo, $i);
+	    	}
+	    }
+
+	    $this->load->view('homepage', $this->getAllData());
+	}
+
+	public function editEvaluation()
+	{
+		$submitInfo = array($this->input->post('evaluation1'), 
+			           		$this->input->post('evaluation2'),
+			           		$this->input->post('evaluation3'),
+			           		$this->input->post('evaluation4'),
+			           		$this->input->post('evaluation5'));
+
+	    $this->load->model('homeModel');
+
+	    for ($i=0; $i < 5; $i++) { 
+	    	if ($submitInfo[$i]) {
+	    		$this->homeModel->editEvaluation($submitInfo, $i);
+	    	}
+	    }
+
+	    $this->load->view('homepage', $this->getAllData());
+	}
+
+	public function getAllData()
+	{
+		$this->load->model('homeModel');
+		$data['introduce'] = $this->homeModel->getIntroduceData();
+		$data['project'] = $this->homeModel->getProjectData();
+		$data['evaluation'] = $this->homeModel->getEvaluationData();
+
+		return $data;
 	}
 }
 
---
+++ admin/application/models/homeModel.php
@@ -0,0 +1,55 @@
+<?php 
+/**
+ * 主页面
+ * @author kevin
+ *
+ */
+class HomeModel extends CI_Model
+{
+
+	function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function editIntroduce($content, $pos)
+    {
+        $this->db->where('position', $pos);
+        $this->db->update('introduce', array('content' => $content));
+    }
+
+    public function editProject($content, $pos)
+    {
+        $this->db->where('position', $pos);
+        $this->db->update('project', array('content' => $content));
+    }
+
+    public function editEvaluation($content, $pos)
+    {
+        $this->db->where('position', $pos);
+        $this->db->update('evaluation', array('content' => $content));
+    }
+
+    public function getIntroduceData()
+    {
+        $this->db->select('content, position');
+        $query = $this->db->get('introduce');
+        return $query->row_array();
+    }
+
+    public function getProjectData()
+    {
+        $this->db->select('content, position');
+        $query = $this->db->get('project');
+        return $query->row_array();
+    }
+
+    public function getEvaluationData()
+    {
+        $this->db->select('content, position');
+        $query = $this->db->get('evaluation');
+        return $query->row_array();
+    }
+}
+
+?>
