2c1816a50ee430386f91d444c677f2aeeb163bc3
sqhtiamo <cloughzhang@cloughzhang-PC0.tencent.com>
2015-04-19 13:02:51 +0800

    界面

---
+++ sooowhat2.sql
@@ -0,0 +1,145 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.9
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: 2015-04-18 13:54:39
+-- 服务器版本： 5.6.20
+-- PHP Version: 5.5.20
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `sooowhat`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `ci_sessions`
+--
+
+CREATE TABLE IF NOT EXISTS `ci_sessions` (
+  `session_id` varchar(40) NOT NULL DEFAULT '0',
+  `ip_address` varchar(16) NOT NULL DEFAULT '0',
+  `user_agent` varchar(120) NOT NULL,
+  `last_activity` int(10) unsigned NOT NULL DEFAULT '0',
+  `user_data` text NOT NULL,
+  PRIMARY KEY (`session_id`),
+  KEY `last_activity_idx` (`last_activity`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- 转存表中的数据 `ci_sessions`
+--
+
+INSERT INTO `ci_sessions` (`session_id`, `ip_address`, `user_agent`, `last_activity`, `user_data`) VALUES
+('32530a48e2c81e0a5b77981d7243da68', '::1', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.118 Safari/537.3', 1429080015, 'a:3:{s:9:"user_data";s:0:"";s:4:"name";s:5:"admin";s:9:"logged_in";b:1;}'),
+('39a4a9bd958f714f1b6a824c745c4618', '::1', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.118 Safari/537.3', 1428907067, 'a:3:{s:9:"user_data";s:0:"";s:4:"name";s:5:"admin";s:9:"logged_in";b:1;}'),
+('cbc2f66b34d409237a0f86ea6b8ae940', '::1', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.118 Safari/537.3', 1428980863, 'a:3:{s:9:"user_data";s:0:"";s:4:"name";s:5:"admin";s:9:"logged_in";b:1;}');
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `evaluation`
+--
+
+CREATE TABLE IF NOT EXISTS `evaluation` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `content` text CHARACTER SET utf8 NOT NULL,
+  `position` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`,`position`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+
+--
+-- 转存表中的数据 `evaluation`
+--
+
+INSERT INTO `evaluation` (`id`, `content`, `position`) VALUES
+(1, 'evaluation5', 1),
+(2, 'evaluation5', 2),
+(4, 'evaluation5', 3),
+(5, 'evaluation5', 4),
+(6, 'evaluation5', 5);
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `introduce`
+--
+
+CREATE TABLE IF NOT EXISTS `introduce` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `content` text CHARACTER SET utf8 NOT NULL,
+  `position` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`,`position`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+
+--
+-- 转存表中的数据 `introduce`
+--
+
+INSERT INTO `introduce` (`id`, `content`, `position`) VALUES
+(1, 'introduce4zzz', 1),
+(2, 'introduce4zzz', 2),
+(4, 'introduce4zzz', 3),
+(5, 'introduce4ssss', 4);
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `project`
+--
+
+CREATE TABLE IF NOT EXISTS `project` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `content` text CHARACTER SET utf8 NOT NULL,
+  `position` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`,`position`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+
+--
+-- 转存表中的数据 `project`
+--
+
+INSERT INTO `project` (`id`, `content`, `position`) VALUES
+(1, 'project5', 1),
+(2, 'project5', 2),
+(3, 'project5', 3),
+(4, 'project5', 4),
+(5, 'project5', 5);
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `user`
+--
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `username` varchar(20) CHARACTER SET utf8 DEFAULT NULL,
+  `password` varchar(20) CHARACTER SET utf8 DEFAULT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- 转存表中的数据 `user`
+--
+
+INSERT INTO `user` (`username`, `password`, `id`) VALUES
+('admin', 'admin', 1);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
